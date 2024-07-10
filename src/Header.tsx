b5b650e27799acebc7016bef4864dbdb07fc4373
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-07-10 10:36:51 +0600

    up

---
+++ src/Header.tsx
@@ -0,0 +1,39 @@
+
+
+import React from 'react'
+import { IMenu } from './App'
+
+interface IHeaderProps {
+    menu: IMenu[]
+}
+
+const Header: React.FC<IHeaderProps> = (props) => {
+    const [content, setContent] = React.useState<string>('Home')
+
+    const handleClick = (block: string) => {
+        setContent(block)
+
+    }
+    return (
+        <div>
+            {props.menu.map((menu) =>
+                <a onClick={
+                    (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+                    e.preventDefault()
+                    handleClick(menu.name)
+                }}
+                    key={menu.name}
+                    href={menu.path}>{menu.name}</a>)}
+                    <br />
+                    <br />
+                    <br />
+            <div>
+                {content === "Home" && <h3>Home Content</h3>}
+                {content === "About" && <h3>About Content</h3>}
+                {content === "Contact" && <h3>Contact Content</h3>}
+            </div>
+        </div>
+    )
+}
+
+export default Header
